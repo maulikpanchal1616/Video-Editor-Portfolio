43f602977c3ff6a7f48df500dc985b1291503490
Maulik <maulikvpanchal2006@gmail.com>
2026-03-06 09:48:37 +0530

    firebase error

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyC8ANBX867lsIfSrcGTa5A4aeVC-JdHrec",
   authDomain: "het-editor.firebaseapp.com",
@@ -13,10 +10,7 @@ const firebaseConfig = {
   appId: "1:965700311297:web:d37bbc647e6fe1a51b6300"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-/* THIS LINE IS IMPORTANT */
+/* export db ONLY ONCE */
 export const db = getFirestore(app);
-
-export { db };
